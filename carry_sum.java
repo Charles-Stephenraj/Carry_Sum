b031a468014967718f89de25e5055224b94347a8
Charles Stephenraj J <123827682+Charles-Stephenraj@users.noreply.github.com>
2023-07-26 22:47:24 +0530

    Add files via upload

---
+++ carry_sum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class carry_sum
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		//int ans=n1+n2;
+		int c=0,r1=0,r2=0,ans=0;
+		while(n1>0 && n2>0)
+		{
+			r1=n1%10;
+			r2=n2%10;
+			ans=r1+r2;
+			if(c>0)
+			{
+				ans=ans+1;
+			}
+			//System.out.println(ans);
+			String a=String.valueOf(ans);
+			if(a.length()>1)
+			{
+				c++;
+				
+			}
+			n1=n1/10;
+			n2=n2/10;
+		}
+		System.out.println(c);
+	}
+}
